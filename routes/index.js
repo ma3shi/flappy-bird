d51403d432af3c99dadb32ece9c9ff6b57e2d518
Masashi Itabashi <masa.itabashi@gmail.com>
2022-01-18 15:38:39 +0900

    ランキング表示

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Result = require("../models/result");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  Result.findAll({ order: [["score", "DESC"]], limit: 5 }).then((results) => {
+    res.render("index", {
+      title: "フラッピーバード",
+      results: results,
+    });
+  });
 });
 
 module.exports = router;
