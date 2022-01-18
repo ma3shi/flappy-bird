d5162f6bab31967c7d544e22d109ee4cbf28c8ed
Masashi Itabashi <masa.itabashi@gmail.com>
2022-01-18 12:34:44 +0900

    sprite,background,sound

--- enemy.js
+++ enemy.js
@@ -3,19 +3,33 @@
 export default class Enemy {
   constructor(game) {
     this.game = game;
+    this.spriteWidth = 522.5; //5225/10
+    this.spriteHeight = 456;
     this.x = this.game.width; //canvas width
     this.y = Math.random() * this.game.height;
-    this.radius = 20; //半径
+    this.randomSize = Math.random() * 0.6 + 0.4; //0.4から1
+    this.radius = 50 * this.randomSize; //半径
+    this.image = new Image();
+    this.image.src = "enemy.png";
+    this.frame = 0; //フレームナンバー
+    this.maxFrame = 10; //最大フレーム数
+    this.frameInterval = 5; //次のフレームに行くまでの間隔
+    this.frameTimer = 0; //次のフレームに行くまでの累積時間
     this.isDeletable = false; //削除可能か
     this.vx = Math.random() * 5 + 5; //速度(x軸)
     this.angle = 0; //Math.sin(angle)で使用
     this.curve = Math.random() * 5 + 5;
     this.isCounted = false; //1つの敵で二回得点しないようにする。
+    this.scoreSound = document.createElement("audio"); //サウンド
+    this.scoreSound.src = "score.mp3"; //サウンド
   }
-  update() {
+
+  //アップデート
+  update(deltaTime) {
     this.x -= this.vx; //左へ
     this.y += Math.sin(this.angle) * this.curve; //上下
     this.angle += 0.12; //角度を加算
+    // this.angle += Math.random() * 0.1 + 0.12; //角度を加算
 
     //左端に行ったらisDeletableをtrueにする
     if (this.x < 0 - this.radius) this.isDeletable = true;
@@ -24,23 +38,50 @@ export default class Enemy {
     const dx = this.x - this.game.player.x; //敵とplayerのx軸の差
     const dy = this.y - this.game.player.y; //敵とplayerのy軸の差
     this.distance = Math.sqrt(dx * dx + dy * dy); //敵とplayerの距離を計算(this.distanceは斜辺)
+
+    // 敵とplayerの距離　< 敵とプレイヤーの半径の合計
     if (this.distance < this.radius + this.game.player.radius) {
-      this.game.isGameOver = true;
+      this.game.isGameOver = true; //ゲームオーバー
     }
 
     //得点
+    //まだカウントしておらず、敵がプレイヤーの左の位置(敵を通過)
     if (!this.isCounted && this.x < this.game.player.x) {
-      this.game.score++;
-      // console.log(`${this.game.score}`);
-
+      this.scoreSound.play(); //音
+      this.game.score++; //スコア
       this.isCounted = true; //1つの敵で二回得点しないようにする。
     }
+
+    //フレーム
+    //プレイヤーと共通なのでまとめられそう
+    if (this.frameTimer > this.frameInterval) {
+      if (this.frame < this.maxFrame) {
+        this.frame++;
+      } else this.frame = 0;
+      this.frameTimer = 0;
+    } else {
+      this.frameTimer += deltaTime;
+    }
   }
+
+  //描画
   draw(ctx) {
-    ctx.fillStyle = "blue";
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.closePath();
+    // ctx.fillStyle = "blue";
+    // ctx.beginPath();
+    // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    // ctx.fill();
+    // ctx.closePath();
+
+    ctx.drawImage(
+      this.image, //img
+      this.frame * this.spriteWidth, //sx
+      0, //sy
+      this.spriteWidth, //sw
+      this.spriteHeight, //sh
+      this.x - 60 * this.randomSize, //dx
+      this.y - 75 * this.randomSize, //dy
+      (this.spriteWidth / 3.5) * this.randomSize, //dw
+      (this.spriteHeight / 3.5) * this.randomSize //dh
+    );
   }
 }
--- script.js
+++ script.js
@@ -1,62 +1,10 @@
-//スコア判定
-
-import Player from "./player.js";
-import Enemy from "./enemy.js";
+import Game from "./game.js";
 
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
-  canvas.width = 600; //cssと同じ
-  canvas.height = 400; //cssと同じ
-
-  class Game {
-    constructor(ctx, width, height) {
-      this.ctx = ctx;
-      this.width = width; //canvasの高さ
-      this.height = height; //canvasの幅
-      this.isPressSpace = false; //スペースキーを押したか
-      this.player = new Player(width, height); //プレイヤー
-      this.enemies = []; //敵の配列
-      this.enemyInterval = 500; //敵を作成する間隔
-      this.enemyTimer = 0; //次の敵を作成するまでの累積時間
-      this.score = 0; //スコア
-      this.isGameOver = false; //ゲームオーバーかどうか
-    }
-    update(deltaTime) {
-      //isDeletable(削除可能か)がfalseのものだけにする。
-      this.enemies = this.enemies.filter((enemy) => !enemy.isDeletable);
-      // 次の敵を作成するまでの累積時間>敵を作成する間隔
-      if (this.enemyTimer > this.enemyInterval) {
-        this.#addNewEnemy(); //敵作成
-        this.enemyTimer = 0; //次の敵を作成するまでの累積時間をリセット
-        // console.log(this.enemies);
-      } else {
-        this.enemyTimer += deltaTime; //frame間のミリ秒を次の敵を作成するまでの累積時間に加える
-      }
-      this.enemies.forEach((enemy) => enemy.update()); //敵更新
-      this.player.update(this.isPressSpace); //プレイヤー更新
-    }
-    draw() {
-      this.enemies.forEach((enemy) => enemy.draw(this.ctx));
-      this.player.draw(this.ctx);
-      this.ctx.font = "50px Georgia";
-      this.ctx.fillText(this.score, 50, 70);
-    }
-    //敵を作成　private class method
-    #addNewEnemy() {
-      this.enemies.push(new Enemy(this));
-    }
-    drawGameOver() {
-      this.ctx.textAlign = "center";
-      this.ctx.fillStyle = "blace";
-      this.ctx.fillText(`GAME OVER`, this.width / 2, this.height / 2);
-      this.ctx.fillText(
-        `your score is ${this.score}`,
-        this.width / 2,
-        this.height / 2 + 50
-      );
-    }
-  }
+  canvas.width = this.window.innerWidth;
+  canvas.height = this.window.innerHeight;
 
   const game = new Game(ctx, canvas.width, canvas.height);
 
@@ -70,9 +18,12 @@ window.addEventListener("load", function () {
     lastTime = timeStamp; //次のフレーム用にタイムスタンプを直前の時間として代入
     game.update(deltaTime); //deltaTimeはenemy作成に使用
     game.draw();
-
-    if (!game.isGameOver) requestAnimationFrame(animation);
-    else game.drawGameOver();
+    if (!game.isGameOver) {
+      requestAnimationFrame(animation);
+    } else {
+      game.drawGameOver();
+      game.gameOverSound.play();
+    }
   }
   animation(0); //引数に何も入れないと最初がundefinedでNaNになってしまう
 
@@ -81,7 +32,7 @@ window.addEventListener("load", function () {
     // console.log(e);
     if (e.code === "Space") game.isPressSpace = true;
   });
-  //スペースキーを離す
+  //スペースキーを押さない
   window.addEventListener("keyup", function (e) {
     if (e.code === "Space") game.isPressSpace = false;
   });
